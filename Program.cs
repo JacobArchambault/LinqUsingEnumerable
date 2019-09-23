37e752fa6858d4280fe27cb383a57e6f876fd7f0
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-23 09:42:08 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqUsingEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueryStringsWithOperators();
+
+            Console.ReadLine();
+        }
+        static void QueryStringsWithOperators()
+        {
+            Console.WriteLine("***** Using query operators *****");
+
+            string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2" };
+
+            var subset = from game in currentVideoGames where game.Contains(" ") orderby game select game;
+
+            foreach (string s in subset)
+                Console.WriteLine("Item: {0}", s);
+        }
+    }
+}
